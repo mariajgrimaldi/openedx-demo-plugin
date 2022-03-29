62724e4df238d28977beb7d562a3946f9cce42d6
Maria Grimaldi <maria.grimaldi@edunext.co>
2022-03-29 13:35:40 -0400

    feat: add plugin configuration settings

---
+++ openedx_demo_plugin/settings/common.py
@@ -0,0 +1,11 @@
+"""
+Common settings for the Open edX Demo site.
+"""
+
+def plugin_settings(settings):
+    """
+    Defines openedx-demo-plugin settings when app is used as a plugin to edx-platform.
+    See: https://github.com/openedx/edx-django-utils/tree/master/edx_django_utils/plugins
+    """
+    settings.OPEN_EDX_VISITOR_ORG = "Public"
+    settings.FEATURES['ENABLE_CREATOR_GROUP'] = True
