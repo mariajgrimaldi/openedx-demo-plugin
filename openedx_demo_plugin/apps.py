9bd60a9d0aad9b23a33c5764fdf363dc15164990
Maria Grimaldi <maria.grimaldi@edunext.co>
2022-03-29 13:33:26 -0400

    feat: add app configuration for settings and signals receiver

--- openedx_demo_plugin/apps.py
+++ openedx_demo_plugin/apps.py
@@ -11,3 +11,29 @@ class OpenedxDemoPluginConfig(AppConfig):
     """
 
     name = 'openedx_demo_plugin'
+
+    plugin_app = {
+        'signals_config': {
+            'lms.djangoapp': {
+                'relative_path': 'receivers',
+                'receivers': [
+                    {
+                        'receiver_func_name': 'assign_org_course_access_to_user',
+                        'signal_path': 'openedx_events.learning.signals.STUDENT_REGISTRATION_COMPLETED',
+                    },
+                ],
+            }
+        },
+        'settings_config': {
+            'lms.djangoapp': {
+                'test': {'relative_path': 'settings.test'},
+                'common': {'relative_path': 'settings.common'},
+                'devstack': {'relative_path': 'settings.devstack'},
+            },
+            'cms.djangoapp': {
+                'test': {'relative_path': 'settings.test'},
+                'common': {'relative_path': 'settings.common'},
+                'devstack': {'relative_path': 'settings.devstack'},
+            },
+        },
+    }
