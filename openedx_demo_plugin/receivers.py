d6b8c4cd929ea190e9c498037e60d7a9b60dbc7f
Maria Grimaldi <maria.grimaldi@edunext.co>
2022-09-26 15:22:42 -0400

    feat!: add mango (maple) support for course creation assignment receiver (#1)
    This PR adds support for our maple fork, since the issue BC-14 is not available in that version.
    
    Previously, we'd implemented the automatic permissions assignment based on the customization BC-14 that was implemented in our lilac fork. Now, we're making it compatible with maple using the course creator model.

--- openedx_demo_plugin/receivers.py
+++ openedx_demo_plugin/receivers.py
@@ -4,16 +4,21 @@ File where Open edX Events receivers are defined.
 For a detailed description on events receivers definitions please refer to the
 hooks official documentation.
 """
+import logging
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from openedx_events.learning.data import UserData
 
 try:
-    from common.djangoapps.student.api import get_access_role_by_role_name
+    from cms.djangoapps.course_creators.models import CourseCreator
+    from organizations.api import get_organization_by_short_name
 except ImportError:
-    get_access_role_by_role_name = object
+    get_organization_by_short_name = object
+    CourseCreator = object
 
 User = get_user_model()
+log = logging.getLogger(__name__)
 
 
 def assign_org_course_access_to_user(user: UserData, **kwargs):
@@ -24,10 +29,31 @@ def assign_org_course_access_to_user(user: UserData, **kwargs):
     OPEN_EDX_VISITOR_ORG setting if exists. If doesn't exist, then acts
     like a noop.
     """
-    visitor_org = getattr(settings, "OPEN_EDX_VISITOR_ORG", None)
-    if not visitor_org:
+    visitor_org_short_name = getattr(settings, "OPEN_EDX_VISITOR_ORG", None)
+    if not visitor_org_short_name:
+        log.info("No OPEN_EDX_VISITOR_ORG provided, terminating course creation assignment.")
+        return
+
+    course_creator_admin_id = getattr(settings, "COURSE_CREATOR_ADMIN_ID", None)
+    if not course_creator_admin_id:
+        log.info("No COURSE_CREATOR_ADMIN_ID provided, terminating course creation assignment.")
         return
 
     registered_user = User.objects.get(username=user.pii.username)
-    org_content_creator_role = get_access_role_by_role_name("org_course_creator_group")
-    org_content_creator_role(org=visitor_org).add_users(registered_user)
+    course_creator = CourseCreator(
+        user=registered_user,
+        state=CourseCreator.GRANTED,
+        all_organizations=False,
+    )
+
+    # In order to add course creator permissions programmatically, we must attach
+    # to the user just registered. So the post_add signals receivers like
+    # `course_creator_organizations_changed_callback` can run checks over instance.admin.
+    visitor_org = get_organization_by_short_name(visitor_org_short_name)
+    try:
+        course_creator.admin = User.objects.get(username=course_creator_admin_id)
+    except User.DoestNotExist:
+        log.exception("User with username specified in COURSE_CREATOR_ADMIN_ID does not exist.")
+        return
+    course_creator.save()
+    course_creator.organizations.add(visitor_org.get("id"))
